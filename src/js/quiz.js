7235e451e8ddbc1fe661d323a5f3a74885b23576
Beata <beata.bartczak34@gmail.com>
2023-01-03 18:22:05 +0100

    finished

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -3,69 +3,11 @@ const formBox = document.querySelectorAll('.page7-form-box')
 const formPrevBtn = document.querySelector('.page7-btn-prev')
 const formNextBtn = document.querySelector('.page7-btn-next')
 const submitBtn = document.querySelector('.page7-btn-submit')
-const quizError = document.querySelector('.page7-error') 
-
+const quizError = document.querySelector('.page7-error')
+const score = document.querySelector('.page7-final-text')
+const finalPage = document.querySelector('.page7-final')
 let count = 1
 
-const questions = [
-	{
-		question: 'Jak brzmi łacińska nazwa pszczoły miodnej?',
-		answers: {
-			a: 'Apis mellifera',
-			b: 'Apis dorsata',
-			c: 'Apis florea',
-		},
-		correctAnswer: 'a',
-		name: 'name',
-	},
-	{
-		question: 'Jakiego koloru nie widzą pszczoły?',
-		answers: {
-			a: 'Żółtego',
-			b: 'Czerwonego',
-			c: 'Zielonego',
-		},
-		correctAnswer: 'b',
-		name: 'color',
-	},
-	{
-		question: 'Co pszczoły komunikują tańcem wywijanym?',
-		answers: {
-			a: 'Źródło pożytku',
-			b: 'Zagrożenie',
-			c: 'Radość',
-		},
-		correctAnswer: 'a',
-		name: 'dance',
-	},
-	{
-		question: 'W jakim wieku pszczoła robotnica zajmuje się budową wosku?',
-		answers: {
-			a: '1-3',
-			b: '7-14',
-			c: '15-18',
-		},
-		correctAnswer: 'c',
-		name: 'worker',
-	},
-]
-
-// const createQuiz = () => {
-// form.innerHTML=`   <h3 class="page7-form-title">${questions[count].question}</h3>
-// <div class="page7-form-question">
-//     <input id="${questions[count].name}" type="radio" class="page7-form-question-input">
-//     <label name="${questions[count].name}">${questions[count].answers.a}</label>
-// </div>
-// <div class="page7-form-question">
-//     <input id="${questions[count].name}" type="radio" class="page7-form-question-input">
-//     <label name="${questions[count].name}">${questions[count].answers.b}</label>
-// </div>
-// <div class="page7-form-question">
-//     <input id="${questions[count].name}" type="radio" class="page7-form-question-input">
-//     <label name="${questions[count].name}">${questions[count].answers.c}</label>
-// </div>`
-
-// }
 const handleformNextBtn = () => {
 	if (count >= 1 && count < formBox.length) {
 		count++
@@ -77,7 +19,7 @@ const handleformNextBtn = () => {
 			}
 		})
 	}
-    disableBtns()
+	disableBtns()
 }
 const handleformPrevBtn = () => {
 	if (count > 1 && count <= formBox.length) {
@@ -90,7 +32,7 @@ const handleformPrevBtn = () => {
 			}
 		})
 	}
-    disableBtns()
+	disableBtns()
 }
 
 const disableBtns = () => {
@@ -99,28 +41,58 @@ const disableBtns = () => {
 	} else if (count === 1) {
 		formPrevBtn.disabled = true
 	} else {
-        formNextBtn.disabled=false
-        formPrevBtn.disabled=false
-    }
+		formNextBtn.disabled = false
+		formPrevBtn.disabled = false
+	}
 }
 
 const answersArray = []
 
+const correctAnswers = ['Apis mellifera', 'Czerwonego', 'Źródło pożytku', '15-18 dni']
+
 const handleSubmitBtn = () => {
- const checkedName = document.querySelector('input[name = "name"]:checked')
- const checkedColor = document.querySelector('input[name = "color"]:checked')
- const checkedDance = document.querySelector('input[name = "dance"]:checked')
- const checkedAge = document.querySelector('input[name = "age"]:checked')
+	const checkedName = document.querySelector('input[name = "name"]:checked')
+	const checkedColor = document.querySelector('input[name = "color"]:checked')
+	const checkedDance = document.querySelector('input[name = "dance"]:checked')
+	const checkedAge = document.querySelector('input[name = "age"]:checked')
 
-if(checkedColor != null && checkedName != null && checkedAge != null && checkedDance != null ) {
-    answersArray.push(checkedName.nextElementSibling.innerHTML, checkedColor.nextElementSibling.innerHTML, checkedDance.nextElementSibling.innerHTML, checkedAge.nextElementSibling.innerHTML)
-    console.log(answersArray);
-} else {
-quizError.textContent='Musisz zaznaczyć wszystkie odpowiedzi!'
+	if (checkedColor != null && checkedName != null && checkedAge != null && checkedDance != null) {
+		answersArray.push(
+			checkedName.nextElementSibling.innerHTML,
+			checkedColor.nextElementSibling.innerHTML,
+			checkedDance.nextElementSibling.innerHTML,
+			checkedAge.nextElementSibling.innerHTML
+		)
+		console.log(answersArray)
+		checkAnswers()
+	
+
+	} else {
+		quizError.textContent = 'Musisz zaznaczyć wszystkie odpowiedzi!'
+	}
 }
 
-
+const checkAnswers = () => {
+	let correctNum = 0
+	for (let i = 0; i < 4; i++) {
+		if (answersArray[i] === correctAnswers[i]) {
+			correctNum++
+		}
+	}
+	showScore(correctNum)
 }
+
+const showScore = (num) => {
+	formBox.forEach(box => {
+		box.classList.remove('active-question')})
+finalPage.classList.add('active-question')
+	score.textContent=`${num}/4 poprawnych odpowiedzi`
+	submitBtn.style.display = 'none'
+	formPrevBtn.style.display = 'none'
+	formNextBtn.style.display = 'none'
+	
+}
+
 submitBtn.addEventListener('click', handleSubmitBtn)
 formNextBtn.addEventListener('click', handleformNextBtn)
 formPrevBtn.addEventListener('click', handleformPrevBtn)
